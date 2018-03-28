986c93d72fb528bd8722d24e1d63006ee6c45b63
Sachin Saxena <spycon007@gmail.com>
2018-03-28 17:35:26 +0530

    comment added

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@ $.ajaxSetup({
 });
 
 d3.json('/product_quantity', function(data){
+  //implement chart functionality here
   $('#product_quantity_chart').empty();
   $('#product_quantity_chart').html(_.templateFromUrl('/snippets.html', '#product_quantity_block')(
     {'result': data}));
