334e52c6c83037283f131e3689fb86ccf181bd4c
Sachin Saxena <spycon007@gmail.com>
2018-03-28 17:34:32 +0530

    sample function added

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 from gramex.handlers import BaseHandler
 
 
+def hello_world(handler):
+    '''
+    Implement data operations here
+    '''
+    print("hello")
 class Hello(BaseHandler):
     def get(self):
         self.write('hello world, this is hello from gramex')
